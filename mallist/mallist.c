6964fec86ee9733d83a17738c64a236dc7c4d4d0
leonel-giuliano <129548024+leonel-giuliano@users.noreply.github.com>
2024-05-10 17:01:50 -0300

    New file handler

--- mallist/mallist.c
+++ mallist/mallist.c
@@ -1,15 +1,18 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "mallist.h"
 
-static memoryArray_t *memoryArray = NULL;
+static memoryArray_t *memoryArray = NULL;   /* List for memory */
+static memoryArray_t *fileArray = NULL;     /* List for files */
 
+// DINAMICALLY ALLOCATED MEMORY
 void *mallist(size_t size) {
     void *ptrTemp;      /* Pointer withouth a definition type */
-    if((ptrTemp = malloc(size)) == NULL) memoryError();
+    if((ptrTemp = malloc(size)) == NULL) errorHandler(MEMORY_ERROR);
 
     memoryArray_t *newPtr;
-    if((newPtr = (memoryArray_t*)malloc(sizeof(memoryArray_t))) == NULL) memoryError();
+    if((newPtr = (memoryArray_t*)malloc(sizeof(memoryArray_t))) == NULL) errorHandler(MEMORY_ERROR);
 
     newPtr->ptr = ptrTemp;
     newPtr->next = memoryArray;
@@ -49,9 +52,61 @@ void freePtr(void *delPtr) {
     free(iPtr);
 }
 
-void memoryError() {
-    printf("There is no memory left.\n");
+
+
+// FILES
+FILE *fopenList(const char *path, const char *mode) {
+    FILE *file;
+    if((file = fopen(path, mode)) == NULL) errorHandler(FILE_ERROR);
+    // Opens the file in the set mode
+
+    memoryArray_t *newFile;
+    if((newFile = (FILE *)malloc(sizeof(memoryArray_t))) == NULL) errorHandler(FILE_ERROR);
+
+    newFile->ptr = file;
+    newFile->next = fileArray;
+    fileArray = newFile;
+    // Adds the file to the list
+
+    return file;
+}
+
+void fcloseAll() {
+    memoryArray_t *closeFile;
+
+    while(fileArray != NULL) {
+        closeFile = fileArray;
+        fileArray = fileArray->next;
+        // Detaches the file
+
+        fclose(closeFile->ptr);
+        free(closeFile);
+        // One closes the file and the other frees the element from the list
+    }
+}
+
+
+
+// ERROR HANDLER
+void errorHandler(error_t event) {
+    printf("ERROR: ");
+
+    switch(event) {
+        case MEMORY_ERROR:
+            printf("There was a problem with the allocation.\n");
+            break;
+
+        case FILE_ERROR:
+            printf("There was a problem with the file.\n");
+            break;
+
+        default:
+            printf("Unknown reason.\n");
+            break;
+    }
+
     freeAll();
+    fcloseAll();
 
     exit(EXIT_FAILURE);
 }
--- mallist/mallist.h
+++ mallist/mallist.h
@@ -1,15 +1,30 @@
 #ifndef __MALLIST_H__
 #define __MALLIST_H__
 
+typedef enum _error {
+    MEMORY_ERROR,
+    FILE_ERROR
+}error_t;
+// Enumeration for the error events
+
 typedef struct _memoryArray {
     void *ptr;
     struct _memoryArray *next;
 }memoryArray_t;
 // Array for all the dynamic allocated memories
 
+// DINAMICALLY ALLOCATED MEMORY
 void *mallist(size_t);      /* Allocates the file inside the array, replaces malloc() */
 void freeAll();             /* Frees all the allocated memories instances */
 void freePtr(void *);       /* Frees only the pointer given, replaces free() */
-void memoryError(void);     /* ERROR: malloc() == NULL */
+
+// FILES
+FILE *fopenList(const char*, const char*);
+void fcloseAll(void);
+
+
+
+// ERROR HANDLER
+void errorHandler(error_t);
 
 #endif
